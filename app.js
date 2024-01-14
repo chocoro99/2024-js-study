0c21cd84c5840783fa9cc7fe84806402a13bd9fe
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2024-01-14 17:30:41 +0900

    feat: variables

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert("hi~");
+// alert("hi~");
+
+console.log(5343323);
+console.log("leee");
+
+console.log(5 + 2);
+console.log(5 * 2);
+console.log(5 / 2);
+
+const a = 5;
+const b = 2;
+const myName = "nico";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello" + myName);
