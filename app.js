34c273ef79253140c14c732fc1f35901d0f0403a
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2024-01-17 23:10:38 +0900

    feat: array

--- app.js
+++ app.js
@@ -39,3 +39,22 @@ console.log(amIFat);
 let blank = null;
 let something;
 console.log(something, blank);
+
+// array
+// const mon = "mon";
+// const tue = "tue";
+// const wed = "wed";
+// const thu = "thu";
+// const fri = "fri";
+// const sat = "sat";
+// const sun = "sun";
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get Item from Array
+console.log(daysOfWeek[5]);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
